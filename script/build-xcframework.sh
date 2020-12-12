3acbb1db8662da393a16e56697256243ce37cf5b
Lakr Aream <master@233owo.com>
2020-12-12 20:15:08 +0800

    Optimization for bitcode generation
    Optimization for bitcode generation
    
    - Enable Bitcode
    - Update to libssh2 1.10.0
    - Update to OpenSSL to tag OpenSSL_1_1_1l

--- script/build-xcframework.sh
+++ script/build-xcframework.sh
@@ -14,11 +14,12 @@ fetchSource () {
   mkdir -p "$path"
   echo "Downloading $filename"
   curl -L -S -s "$url" --output "$file"
-  local md5=`md5 -q $file`
+  local md5
+  md5=$(md5 -q "$file")
   echo "MD5: $md5"
 
   tar -zxkf "$file" -C "$path" --strip-components 1 2>&-
-  rm -f $file
+  rm -f "$file"
 }
 
 buildLibrary () {
@@ -42,17 +43,19 @@ set -e
 
 #Config
 
-export BUILD_THREADS=$(sysctl hw.ncpu | awk '{print $2}')
-LIBSSH_TAG=1.9.0
-LIBSSL_TAG=OpenSSL_1_1_1h
+BUILD_THREADS=$(sysctl hw.ncpu | awk '{print $2}')
+export BUILD_THREADS
+LIBSSH_TAG=1.10.0
+LIBSSL_TAG=OpenSSL_1_1_1l
 
 TAG=$LIBSSH_TAG+$LIBSSL_TAG
 ZIPNAME=CSSH-$TAG.xcframework.zip
-GIT_REMOTE_URL_UNFINISHED=`git config --get remote.origin.url|sed "s=^ssh://==; s=^https://==; s=:=/=; s/git@//; s/.git$//;"`
+GIT_REMOTE_URL_UNFINISHED=$(git config --get remote.origin.url|sed "s=^ssh://==; s=^https://==; s=:=/=; s/git@//; s/.git$//;")
 DOWNLOAD_URL=https://$GIT_REMOTE_URL_UNFINISHED/releases/download/$TAG/$ZIPNAME
 
-export ROOT_PATH=$(cd "$(dirname "$0")/.."; pwd -P)
-pushd $ROOT_PATH > /dev/null
+ROOT_PATH=$(cd "$(dirname "$0")/.."; pwd -P)
+export ROOT_PATH
+pushd "$ROOT_PATH" > /dev/null
 
 export BUILD=$ROOT_PATH/build
 export TEMPPATH=$ROOT_PATH/temp
@@ -69,7 +72,6 @@ if [[ -d "$OPENSSL_SOURCE" ]] && [[ -d "$LIBSSH_SOURCE" ]]; then
 else
   fetchSource "https://github.com/libssh2/libssh2/releases/download/libssh2-$LIBSSH_TAG/libssh2-$LIBSSH_TAG.tar.gz" "libssh2.tar.gz" "$LIBSSH_SOURCE"
   fetchSource "https://github.com/openssl/openssl/archive/$LIBSSL_TAG.tar.gz" "openssl.tar.gz" "$OPENSSL_SOURCE"
-  patch -d "$OPENSSL_SOURCE" -p 0 -i "$ROOT_PATH/script/patch-ssl.txt"
 fi
 
 #Build
@@ -82,37 +84,37 @@ fi
 #export ARCHS=$5
 #export MIN_VERSION=$6
 
-buildLibrary "$BUILD/maccatalyst" "macosx" "MacOSX" "-maccatalyst" "x86_64 arm64" "10.15"
 buildLibrary "$BUILD/iphoneos" "iphoneos" "iPhoneOS" "" "armv7 armv7s arm64" "9.0"
 buildLibrary "$BUILD/iphonesimulator" "iphonesimulator" "iPhoneSimulator" "" "x86_64 arm64" "9.0"
 buildLibrary "$BUILD/macosx" "macosx" "MacOSX" "" "x86_64 arm64" "10.10"
+buildLibrary "$BUILD/maccatalyst" "macosx" "MacOSX" "-maccatalyst" "x86_64 arm64" "10.15"
 buildLibrary "$BUILD/appletvsimulator" "appletvsimulator" "AppleTVSimulator" "" "x86_64 arm64" "9.0"
 buildLibrary "$BUILD/appletvos" "appletvos" "AppleTVOS" "" "arm64" "9.0"
 buildLibrary "$BUILD/watchsimulator" "watchsimulator" "WatchSimulator" "" "x86_64 arm64" "2.0"
 buildLibrary "$BUILD/watchos" "watchos" "WatchOS" "" "armv7k arm64_32" "2.0"
 
 xcodebuild -create-xcframework \
- -library $BUILD/macosx/lib/libssh2.a \
- -headers $BUILD/macosx/include \
- -library $BUILD/iphoneos/lib/libssh2.a \
- -headers $BUILD/iphoneos/include \
- -library $BUILD/iphonesimulator/lib/libssh2.a \
- -headers $BUILD/iphonesimulator/include \
- -library $BUILD/maccatalyst/lib/libssh2.a \
- -headers $BUILD/maccatalyst/include \
- -library $BUILD/appletvsimulator/lib/libssh2.a \
- -headers $BUILD/appletvsimulator/include \
- -library $BUILD/appletvos/lib/libssh2.a \
- -headers $BUILD/appletvos/include \
- -library $BUILD/watchsimulator/lib/libssh2.a \
- -headers $BUILD/watchsimulator/include \
- -library $BUILD/watchos/lib/libssh2.a \
- -headers $BUILD/watchos/include \
+ -library "$BUILD/macosx/lib/libssh2.a" \
+ -headers "$BUILD/macosx/include" \
+ -library "$BUILD/iphoneos/lib/libssh2.a" \
+ -headers "$BUILD/iphoneos/include" \
+ -library "$BUILD/iphonesimulator/lib/libssh2.a" \
+ -headers "$BUILD/iphonesimulator/include" \
+ -library "$BUILD/maccatalyst/lib/libssh2.a" \
+ -headers "$BUILD/maccatalyst/include" \
+ -library "$BUILD/appletvsimulator/lib/libssh2.a" \
+ -headers "$BUILD/appletvsimulator/include" \
+ -library "$BUILD/appletvos/lib/libssh2.a" \
+ -headers "$BUILD/appletvos/include" \
+ -library "$BUILD/watchsimulator/lib/libssh2.a" \
+ -headers "$BUILD/watchsimulator/include" \
+ -library "$BUILD/watchos/lib/libssh2.a" \
+ -headers "$BUILD/watchos/include" \
  -output CSSH.xcframework
 
 zip --recurse-paths -X --quiet $ZIPNAME CSSH.xcframework
 rm -rf CSSH.xcframework
-CHECKSUM=`shasum -a 256 -b $ZIPNAME | awk '{print $1}'`
+CHECKSUM=$(shasum -a 256 -b $ZIPNAME | awk '{print $1}')
 
 cat >Package.swift << EOL
 // swift-tools-version:5.3
