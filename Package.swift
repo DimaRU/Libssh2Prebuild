21b22714ebe0c9d9ec645c7d0550825ac4d36db9
Lakr Aream <master@233owo.com>
2021-09-24 09:51:09 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,6 +8,8 @@ let package = Package(
         .library(name: "CSSH", targets: ["CSSH"])
     ],
     targets: [
-        .binaryTarget(name: "CSSH", path: "build.xcframework")
+        .binaryTarget(name: "CSSH",
+                      url: "https://github.com/Co2333/Libssh2Prebuild/releases/download/1.10.0+OpenSSL_1_1_1l/CSSH-1.10.0+OpenSSL_1_1_1l.xcframework.zip",
+                      checksum: "85177cf5449081553789e9961ddffe9961e970d82980bd84a57676a17fa59a3a")
     ]
 )
