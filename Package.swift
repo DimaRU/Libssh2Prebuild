3f8a255db9d3286ca3447c0f596746d60f25dff5
Dmitriy Borovikov <dmitry@borovikov.name>
2023-07-06 13:51:57 +0300

    Build 1.11.0+OpenSSL_1_1_1u+beta

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "CSSH",
-                      url: "https://github.com/DimaRU/Libssh2Prebuild/releases/download/1.11.0+OpenSSL_1_1_1u/CSSH-1.11.0+OpenSSL_1_1_1u.xcframework.zip",
-                      checksum: "c5d8d6843149bcc1d6fd719b25f504612204fb4e91df4d842307b7fb88a953a8")
+                      url: "https://github.com/DimaRU/Libssh2Prebuild/releases/download/1.11.0+OpenSSL_1_1_1u+beta/CSSH-1.11.0+OpenSSL_1_1_1u+beta.xcframework.zip",
+                      checksum: "7964bb946da1424e2b25497c2cab73185a32c105716c6c69cb622e1cacaa4000")
     ]
 )
